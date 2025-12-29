19cab6b8752c80b8d36545e94c694d2d5d115dcc
Madhumitha212 <madhuravi324@gmail.com>
2025-12-29 20:04:24 +0530

    setup:[Madhu:] - created child classes to implement inheritance

--- source/vehicle.py
+++ source/vehicle.py
@@ -22,3 +22,4 @@ class ElectricScooter(Vehicle):
     def __init__(self, vehicle_id, model, battery_percentage,max_speed_limit):
         super().__init__(vehicle_id, model, battery_percentage)
         self.max_speed_limit = max_speed_limit
+
