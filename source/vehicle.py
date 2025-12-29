e6fc101b7a08efae5bf0de46a09b04458c5b869e
Madhumitha212 <madhuravi324@gmail.com>
2025-12-29 19:56:14 +0530

    setup:[Madhu:] - Added private variables

--- source/vehicle.py
+++ source/vehicle.py
@@ -3,3 +3,12 @@ class Vehicle:
         self.vehicle_id = vehicle_id
         self.model = model
         self.battery_percentage = battery_percentage
+        self.set_battery_percentage(battery_percentage)
+
+    def get_battery_percentage(self):
+        return self.__battery_percentage
+    def set_battery_percentage(self,battery_percentage):
+        if 0<=battery_percentage <= 100:
+            self.__battery_percentage = battery_percentage
+        else:
+            print("Invalid battery percentage! Must be between 0 and 100:")
